29070e9299fe6f731586c66aeaebde56e7839fe6
Magical Octopus <aiqing.shi@outlook.com>
2026-02-01 19:33:50 -0800

    Release v0.1.5

--- Formula/octo-code.rb
+++ Formula/octo-code.rb
@@ -1,15 +1,15 @@
 class OctoCode < Formula
   desc "Voice-driven multi-agent development environment"
   homepage "https://github.com/OASans/homebrew-octo-code-release"
-  version "0.1.4"
+  version "0.1.5"
   license "MIT"
 
   depends_on "tmux"
   depends_on arch: :arm64
   depends_on :macos
 
-  url "https://github.com/OASans/homebrew-octo-code-release/releases/download/v0.1.4/octo-code-0.1.4-aarch64-apple-darwin.tar.gz"
-  sha256 "98da9c745e8bc73dd95fcaaa8cabbb32dad22bf05c9869acb88083565bcd4ea8"
+  url "https://github.com/OASans/homebrew-octo-code-release/releases/download/v0.1.5/octo-code-0.1.5-aarch64-apple-darwin.tar.gz"
+  sha256 "43dfc1e2ae8ddc62574a577b3060930ca32c9a38dac805e630fa2efb1c83e8c2"
 
   def install
     bin.install "octo-code"
