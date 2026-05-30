fca4db07f73480a45e28c03b35fa774a21348615
Magical Octopus <aiqing.shi@outlook.com>
2026-05-30 14:25:13 -0700

    Release v0.1.667

--- Formula/octo-code.rb
+++ Formula/octo-code.rb
@@ -11,7 +11,7 @@ class OctoCode < Formula
   depends_on :macos
 
   url "https://github.com/OASans/homebrew-octo-code-release/releases/download/v0.1.667/octo-code-0.1.667-aarch64-apple-darwin.tar.gz"
-  sha256 "331269fad5e506eaf972ed072b2240298b8392a91a7d12bdda7c0aca2260d3e8"
+  sha256 "083b0e907e3445b02cfbb5342736b34ae069c1e7f9ed28efc3049be07cc73768"
 
   def install
     bin.install "octo-code"
